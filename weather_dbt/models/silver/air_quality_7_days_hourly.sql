6dc7f93c3ae7818ad15eed8178bc02dab9162623
ahmad <m.ahmad03x08@gmail.com>
2026-08-12 14:06:58 +0200

    Removed an extra join the silver layer for City_id

--- weather_dbt/models/silver/air_quality_7_days_hourly.sql
+++ weather_dbt/models/silver/air_quality_7_days_hourly.sql
@@ -6,33 +6,21 @@
 
 WITH air_quality AS (
     SELECT
-        payload.latitude,
-        payload.longitude,
+        city_id,
         UNNEST(payload.hourly.time) AS observation_time,
         UNNEST(payload.hourly.european_aqi) AS european_aqi,
         UNNEST(payload.hourly.us_aqi) AS us_aqi,
         UNNEST(payload.hourly.pm10) AS pm10,
         UNNEST(payload.hourly.pm2_5) AS pm2_5
     FROM {{ ref('air_quality') }}
-),
-
-city_seed AS (
-    SELECT
-        city_id,
-        latitude,
-        longitude
-    FROM {{ ref('city_list') }}
 )
 
 SELECT
-    c.city_id,
-    aq.observation_time::timestamp AS observation_time,
-    aq.european_aqi::integer AS european_aqi,
-    aq.us_aqi::integer AS us_aqi,
-    aq.pm10::double AS pm10,
-    aq.pm2_5::double AS pm2_5,
+    city_id,
+    observation_time::timestamp AS observation_time,
+    european_aqi::integer AS european_aqi,
+    us_aqi::integer AS us_aqi,
+    pm10::double AS pm10,
+    pm2_5::double AS pm2_5,
     current_timestamp AS silver_loaded_at
-FROM air_quality aq
-JOIN city_seed c
-    ON round(aq.latitude, 1) = round(c.latitude, 1)
-    AND round(aq.longitude, 1) = round(c.longitude, 1)
+FROM air_quality
--- weather_dbt/models/silver/weather_7_days_hourly.sql
+++ weather_dbt/models/silver/weather_7_days_hourly.sql
@@ -6,30 +6,21 @@
 
 WITH weather AS (
     SELECT
-        payload.latitude AS latitude,
-        payload.longitude AS longitude,
+        city_id,
         UNNEST(payload.hourly.time) AS observation_time,
         UNNEST(payload.hourly.temperature_2m) AS temperature_c,
         UNNEST(payload.hourly.relative_humidity_2m) AS humidity_pct,
         UNNEST(payload.hourly.wind_speed_10m) AS wind_speed_kmh,
         UNNEST(payload.hourly.weather_code) AS weather_code
     FROM {{ ref('weather') }}
-),
-
-city_seed AS (
-    SELECT city_id, latitude, longitude
-    FROM {{ ref('city_list') }}
 )
 
 SELECT
-    c.city_id,
-    w.observation_time::timestamp AS observation_time,
-    w.temperature_c::double AS temperature_c,
-    w.humidity_pct::double AS humidity_pct,
-    w.wind_speed_kmh::double AS wind_speed_kmh,
-    w.weather_code::integer AS weather_code,
+    city_id,
+    observation_time::timestamp AS observation_time,
+    temperature_c::double AS temperature_c,
+    humidity_pct::double AS humidity_pct,
+    wind_speed_kmh::double AS wind_speed_kmh,
+    weather_code::integer AS weather_code,
     current_timestamp AS silver_loaded_at
-FROM weather w
-JOIN city_seed c
-    ON round(w.latitude, 1) = round(c.latitude, 1)
-    AND round(w.longitude, 1) = round(c.longitude, 1)
+FROM weather
